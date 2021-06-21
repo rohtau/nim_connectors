84aa288a1d974c2be0016e383b4102c4d45ede0f
rohtau\pablo <pablo@rohtau.com>
2021-06-21 12:19:50 +0000

    NIM API rohtau version changed to 0.4, was 0.3. still no release tag, but very close...

--- nim_core/__init__.py
+++ nim_core/__init__.py
@@ -5,7 +5,7 @@ import tempfile
 __all__=['nim', 'nim_api', 'nim_file', 'nim_fileUI', 'nim_prefs', 'nim_print', 'nim_win', 'nim_rohtau', 'nim_rohtau_utils']
 
 
-version     = 'v4.0.61 / rohtau v0.3' # Nim API version and rohtau version
+version     = 'v4.0.61 / rohtau v0.4' # Nim API version and rohtau version
 winTitle    = 'NIM_'+version # Global title for all NIM windows
 padding     = 3 # Global padding used for file versions.
 imgpadding  = 4 # Global padding used for images files.
