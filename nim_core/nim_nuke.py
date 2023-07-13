39dd869ddb0d000e7d14969a061ea335113225fb
pablogipi <pablo@rohtau.com>
2023-07-13 22:35:34 +0000

    Fixes in the frame range using handles are working in Houdini, extend o Nuke

--- nim_core/nim_nuke.py
+++ nim_core/nim_nuke.py
@@ -1002,8 +1002,9 @@ def set_globals():
     # nuke.tprint(pformat(shotglobals))
     if 'frames' in shotglobals:
         frames = shotglobals['frames'] if shotglobals['frames'] else default_frame_range
+        handles = shotglobals['handles']
         first = 1001 # We always start at 1001 by convention
-        last = 1001 + frames - 1
+        last = 1001 + frames + (2*handles) - 1
         PS.knob("first_frame").setValue(first)
         PS.knob("last_frame").setValue(last)
         PS.knob('lock_range').setValue(True)
@@ -1012,11 +1013,11 @@ def set_globals():
         for n in viewerNodes:
             n.redraw()
             n.knob('frame_range_lock').setValue(True)
-            viewer_range = "%d-%d"%(first+shotglobals['handles'], (last-shotglobals['handles']))
+            viewer_range = "%d-%d"%(first+handles, (last-handles))
             QTimer.singleShot(100, lambda: setViewerRange(n, viewer_range))
 
-        msg += "- Frame range set to %d-%d. Shot Range (with handles): %d - %d\n"%(first, last, first+shotglobals['handles'], 
-                                                                                   last-shotglobals['handles'])
+        msg += "- Frame range set to %d-%d. Shot Range (with handles): %d - %d\n"%(first, last, first+handles, 
+                                                                                   last-handles)
     else:
         msg += "- WARNING: No Frame Range information for this shot w "
 
