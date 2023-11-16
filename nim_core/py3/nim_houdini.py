947a3e0eb27fd7815d4c82c86b634b93e5fe3e8f
pablogipi <pablo@rohtau.com>
2023-11-16 02:01:11 +0000

    Fix conflict merge in nim_houdini.py

--- nim_core/py3/nim_houdini.py
+++ nim_core/py3/nim_houdini.py
@@ -2,11 +2,7 @@
 #******************************************************************************
 #
 # Filename: nim_houdini.py
-<<<<<<< HEAD
-# Version:  v5.1.2.220314
-=======
 # Version:  v6.1.4.231110
->>>>>>> nimlabs
 #
 # Copyright (c) 2014-2022 NIM Labs LLC
 # All rights reserved.
